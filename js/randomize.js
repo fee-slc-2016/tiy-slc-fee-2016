5de000f9c79dbfc8497e52a6557368685cff5f10
Joni Weiss <joni@handmadepixels.com>
2016-11-16 16:10:09 -0700

    feat(navbar) Add drop-down menu for team members with links

--- js/randomize.js
+++ js/randomize.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var cards = $(".teamMember");
-for(var i = 0; i < cards.length - 1; i++) {
-  var target = Math.floor(Math.random() * cards.length - 1) + 1;
-  var target2 = Math.floor(Math.random() * cards.length - 1) + 1;
-  cards.eq(target).before(cards.eq(target2));
+var navCards = $(".navMember");
+for(var i = 0; i < navCards.length - 1; i++) {
+  var target = Math.floor(Math.random() * navCards.length - 1) + 1;
+  var target2 = Math.floor(Math.random() * navCards.length - 1) + 1;
+  navCards.eq(target).before(navCards.eq(target2));
 }
 
 var carouselCards = $(".carouselRandom");
@@ -12,4 +12,11 @@ for(var i = 0; i < carouselCards.length - 1; i++) {
   var target = Math.floor(Math.random() * carouselCards.length - 1) + 1;
   var target2 = Math.floor(Math.random() * carouselCards.length - 1) + 1;
   carouselCards.eq(target).before(carouselCards.eq(target2));
+}
+
+var cards = $(".teamMember");
+for(var i = 0; i < cards.length - 1; i++) {
+  var target = Math.floor(Math.random() * cards.length - 1) + 1;
+  var target2 = Math.floor(Math.random() * cards.length - 1) + 1;
+  cards.eq(target).before(cards.eq(target2));
 }
